a69b97b01cc714bf7718dde3cc364caa44934e6e
Pedro Munhoz <pedro.munhoz.pessoal@gmail.com>
2024-06-19 20:30:46 +0000

    commit inicial do projeto

---
+++ src/main/java/com/cit/virtual_ponto/cadastro_funcionarios/RegistroPontoFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package com.cit.virtual_ponto.cadastro_funcionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroPontoFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroPontoFuncionarioApplication.class, args);
+	}
+
+}
