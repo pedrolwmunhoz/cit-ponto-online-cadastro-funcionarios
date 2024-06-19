6cb755714c7e65e0f82f280876c068a96d240eaa
Pedro Munhoz <pedro.munhoz.pessoal@gmail.com>
2024-06-19 22:41:47 +0000

    fix FuncionarioEntity

--- src/main/java/com/cit/virtual_ponto/cadastro_funcionarios/models/cadastro_funcionarios/CadastroFuncionariosEntity.java
+++ src/main/java/com/cit/virtual_ponto/cadastro_funcionarios/models/cadastro_funcionarios/CadastroFuncionariosEntity.java
@@ -15,7 +15,7 @@ public class CadastroFuncionariosEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long funcionarioId;
 
     private Long empresaId;
 
