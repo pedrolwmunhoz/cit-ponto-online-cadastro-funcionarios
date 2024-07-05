6d734df58b1243e943c1c3b23e326c1152c09ad4
Pedro Munhoz <pedro.munhoz.pessoal@gmail.com>
2024-07-05 02:04:30 +0000

    validacao campos entrada validarFuncionario

--- src/main/java/com/cit/virtual_ponto/cadastro_funcionarios/dto/LoginRequestDto.java
+++ src/main/java/com/cit/virtual_ponto/cadastro_funcionarios/dto/LoginRequestDto.java
@@ -1,34 +1,19 @@
 package com.cit.virtual_ponto.cadastro_funcionarios.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
 public class LoginRequestDto {
 
+    @NotBlank(message = "Email não pode ser vazio")
+    @Email(message = "Email deve ser válido")
     private String email;
+
+    @NotBlank(message = "Senha não pode ser vazio")
+    @Size(min = 8, max = 100, message = "Senha deve conter 8 ou mais caracteres")
     private String senha;
-
-    // Construtor vazio (obrigatório para o Spring deserializar o JSON)
-    public LoginRequestDto() {
-    }
-
-    // Construtor com os campos
-    public LoginRequestDto(String email, String senha) {
-        this.email = email;
-        this.senha = senha;
-    }
-
-    // Getters e Setters
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
